bacaf0c1032449cc67eaed80f7be8c8395e7d1a7
NammiSanyasi <nammisanyasi4a@gmail.com>
2024-07-08 15:25:04 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'counter';
+  count: number = 0;
+  isDisabled = false;
+  evenFlag = true;
+  isEven ="Even";
+  checkEvenOdd()
+  {
+    if (this.count % 2 == 1) { this.isEven = "odd"; this.evenFlag = false; }
+    else { this.isEven = "Even";this.evenFlag=true }
+  }
+  incr() {
+    if (this.count >= 20) {
+      this.isDisabled=true
+    }
+    else {
+       this.count++;
+    }
+   this.checkEvenOdd()
+
+  }
+  decr() {
+    this.count--;
+  this.checkEvenOdd();}
+  reset() { this.count = 0; }
 }
